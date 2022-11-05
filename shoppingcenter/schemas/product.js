6f49d2db7e16b373f5a92106c8b210f6574043d0
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-11-05 14:40:30 -0500

    created sanity db, connected to FE, showed banner

---
+++ shoppingcenter/schemas/product.js
@@ -0,0 +1,42 @@
+export default {
+  name: "product",
+  title: "Product",
+  type: "document",
+  fields: [
+    {
+      name: "image",
+      title: "Image",
+      type: "array",
+      of: [{ type: "image" }],
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    //unique id
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      //create an id that incoprate the name to make it unique
+      options: {
+        source: "name",
+        maxLength: 90,
+      },
+    },
+    {
+      name: "price",
+      title: "Price",
+      type: "number",
+    },
+    {
+      name: "details",
+      title: "Details",
+      type: "string",
+    },
+  ],
+};
