9855c520b38e81f707195bd640b2dfeecce05643
abdullah al hilfi <abdullahalhilfi21@gmail.com>
2022-11-07 01:49:33 -0600

    fixed stripe key

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -2,7 +2,6 @@ const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    console.log(req.body);
     try {
       const params = {
         submit_type: "pay",
